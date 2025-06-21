5d80ffd9760fbc5c9601170411550b644339de82
Valdir Luiz <arnholdvaldir@gmail.com>
2025-06-21 18:43:45 -0300

    ajustes

--- infra/apigw.tf
+++ infra/apigw.tf
@@ -94,4 +94,28 @@ resource "aws_api_gateway_stage" "prod" {
   stage_name    = "prod"
   rest_api_id   = aws_api_gateway_rest_api.api.id
   deployment_id = aws_api_gateway_deployment.deployment.id
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigw.arn
+    format = jsonencode({
+      requestId     = "$context.requestId"
+      ip            = "$context.identity.sourceIp"
+      caller        = "$context.identity.caller"
+      user          = "$context.identity.user"
+      requestTime   = "$context.requestTime"
+      httpMethod    = "$context.httpMethod"
+      resourcePath  = "$context.resourcePath"
+      status        = "$context.status"
+      protocol      = "$context.protocol"
+      responseLength= "$context.responseLength"
+    })
+  }
+
+  method_settings {
+    resource_path    = "/*"
+    http_method      = "*"
+    logging_level    = "INFO"
+    data_trace_enabled = true
+    metrics_enabled  = true
+  }  
 }
