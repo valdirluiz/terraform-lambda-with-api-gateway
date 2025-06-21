c7aff9084a7657cf49e0190b6f9cfb1fdc7e08b3
Valdir Luiz <arnholdvaldir@gmail.com>
2025-06-21 18:23:09 -0300

    ajuste redirecionamento lambda

--- infra/main.tf
+++ infra/main.tf
@@ -70,7 +70,7 @@ resource "aws_api_gateway_integration" "lambda" {
   rest_api_id = aws_api_gateway_rest_api.api.id
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.proxy.http_method
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.api_lambda.invoke_arn
 }
