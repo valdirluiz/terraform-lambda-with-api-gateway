137e1bba86d1090d14277e16160b4617e51328cd
Valdir Luiz <arnholdvaldir@gmail.com>
2025-06-21 17:24:31 -0300

    alterar nome de bucket s3 do backend do terraform

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "lambda-with-api-gateway-terraform-state"
+    bucket         = "terraform-state-lambda-with-api-gateway"
     key            = "lambda-api/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
